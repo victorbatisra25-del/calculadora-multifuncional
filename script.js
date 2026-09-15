c3cac010e64251b7eaf7841107412f2e2a68b9f9
victorbatisra25-del <victorbatisra25@gmail.com>
2026-09-15 08:46:38 -0300

    +

--- script.js
+++ script.js
@@ -17,23 +17,23 @@ function subtrair() {
 }
 
 function multiplicar() {
-    let numero1 = Number(document.getElementById("Numero 1").value);
-    let numero2 = Number(document.getElementById("Numero 2").value);
+    let numero1 = Number(document.getElementById("numero 1").value);
+    let numero2 = Number(document.getElementById("numero 2").value);
     
     document.getElementById("resultado").innerText =
     "Resultado: " + (numero1 * numero2);
 }
 
 function dividir() {
-    let numero1 = Number(document.getElementById("Numero 1").value);
-    let numero2 = Number(document.getElementById("Numero 2").value);
+    let numero1 = Number(document.getElementById("numero 1").value);
+    let numero2 = Number(document.getElementById("numero 2").value);
         
     if (numero2 === 0) {
-        document.getElementById("Resultado").innerText =
+        document.getElementById("resultado").innerText =
         "Resultado: Não é possível dividir por zero.";
         return;
     }
         
-    document.getElementById("Resultado").innerText =
+    document.getElementById("resultado").innerText =
     "Resultado: " + (numero1 / numero2);
 }
