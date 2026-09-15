5176eafd122df836c19b1c1dcef2c886ed4c443e
victorbatisra25-del <victorbatisra25@gmail.com>
2026-09-15 08:48:14 -0300

    +

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ function subtrair() {
 }
 
 function multiplicar() {
-    let numero1 = Number(document.getElementById("numero 1").value);
-    let numero2 = Number(document.getElementById("numero 2").value);
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
     
     document.getElementById("resultado").innerText =
     "Resultado: " + (numero1 * numero2);
 }
 
 function dividir() {
-    let numero1 = Number(document.getElementById("numero 1").value);
-    let numero2 = Number(document.getElementById("numero 2").value);
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
         
     if (numero2 === 0) {
         document.getElementById("resultado").innerText =
